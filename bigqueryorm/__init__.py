b613480737f9c68c9a16e48ccb9a220696bcc5f3
Johan Nestaas <johannestaas@gmail.com>
2022-10-14 10:18:36 -0700

    imports

--- bigqueryorm/__init__.py
+++ bigqueryorm/__init__.py
@@ -6,7 +6,10 @@ A BigQuery ORM
 
 __title__ = 'bigqueryorm'
 __version__ = '0.0.1'
-__all__ = ()
+__all__ = ("declare_row", "Count")
 __author__ = '__xor__ <dunder.xor.dunder@gmail.com'
 __license__ = 'GPLv3'
 __copyright__ = 'Copyright 2022 __xor__'
+
+
+from .query import declare_row, Count
